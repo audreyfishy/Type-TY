93aaabbe99d2143a64a48153c2c4c324fc833172
audreyfishy <kitazawatakuro@gmail.com>
2022-05-30 03:04:28 +0900

    test updated

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import requests as r
-from bs4 import BeautifulSoup as bs
-res = r.get("https://www.youtube.com/results?search_query=content&sp=EgIQAg%253D%253D")
-soup = bs(res.content, "html.parser")
-print(soup.select("#text-container"))
+import asyncio
+
+async def f():
+    return 100
+
+asyncio.send(f)

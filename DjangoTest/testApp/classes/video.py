6b81757f526fe432e96f459741226c0dabb59d39
audreyfishy <kitazawatakuro@gmail.com>
2022-06-17 00:47:03 +0900

    done

--- DjangoTest/testApp/classes/video.py
+++
@@ -1,9 +0,0 @@
-class Video:
-    # Variables ================================================================
-    # videoID : str
-    # ==========================================================================
-    def __init__(self, videoID : str):
-        self.videoID = videoID
-
-    def toString(self):
-        return self.videoID
